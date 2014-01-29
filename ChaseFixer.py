77fb3134b988168e8de26004963173e2fce2672e
Darien Hager <darien+git@technofovea.com>
2014-01-28 22:17:49 -0800

    Changing name/memo fixing code, may want a more generic "transaction fixing" harness that just modifies a dictionary

--- ChaseFixer.py
+++ ChaseFixer.py
@@ -1,54 +1,61 @@
 #!/usr/bin/python
-## ASSUMES SGML FORMAT FOR CHASE QFX FILES AS OF JAN 2014 (v 102, charset 1252)
 
 import io
 import sys
-from ChaseQfx import QfxToXml, QfxWalker
+from ChaseQfx import QfxToXml, QfxWalker, XmlToQfx
+import xml.etree.ElementTree as ET
 
 
-class ChaseFixer(QfxWalker):
-    def __init__(self):
-        super(ChaseFixer, self).__init__()
-        self.debug = False
-        self.transaction = None
+def fixNameAndMemo(name,memo):
+    return name + memo, ""
 
-    def handleStart(self, tagname):
-        super(ChaseFixer, self).handleStart(tagname)
-        if tagname == "STMTTRN":
-            self.transaction = {}
-            self.transaction["NAME"] = ""
-            self.transaction["MEMO"] = ""
 
-    def handleItem(self, tagname, content):
-        super(ChaseFixer, self).handleItem(tagname, content)
-        if self.transaction is not None:
-            if tagname in self.transaction:
-                self.transaction[tagname] += content
-            else:
-                self.transaction[tagname] = content
+def getNodeText(statementNode, tag):
+    xpath = "./" + tag
+    child = statementNode.find(xpath)
+    if child is None:
+        return ""
+    else:
+        return child.text
 
-    def handleEnd(self, tagname):
-        super(ChaseFixer, self).handleEnd(tagname)
-        if tagname == "STMTTRN":
-            combined = self.transaction["NAME"].strip() + " " + self.transaction["MEMO"].strip()
-            #self.transaction["NAME"] = combined
-            #self.transaction["MEMO"] = ""
-            print combined
-            self.transaction = None
 
+def setNodeText(statementNode, tag, val):
+    xpath = "./" + tag
+    child = statementNode.find(xpath)
+    if child is None:
+        child = ET.SubElement(statementNode, tag)
+    child.text = val
 
 __author__ = 'Darien Hager'
 if __name__ == "__main__":
-    src = io.open('C:\Users\Darien\Documents\YNAB\chase_export\example\checking\JPMC.qfx', 'r', encoding='cp1252')
-    dest = file('out.xml', 'wb')
-    #dest = sys.stdout
+    
+    dir = "C:\\Users\\Darien\\Documents\\YNAB\\chase_export\\example\\checking\\"
+    fid = "JPMC";
+    src = dir + fid + ".qfx"
+    mid = dir + fid + ".xml"
+    fin = dir + fid + "_fixed.qfx"
 
-    #cf = ChaseFixer()
-    #cf.handleFile(f)
+    print src
+    print mid
 
     qx = QfxToXml()
-    qx.handleFile(src)
-    print qx.getXml()
+    qx.handleFile(io.open(src,'r', encoding='cp1252'))
+    qx.getXml(file(mid, 'wb'))
+
+    tree = ET.parse(file(mid, 'rb'))
+    statementNodes = tree.findall("./OFX/BANKMSGSRSV1/STMTTRNRS/STMTRS/BANKTRANLIST/STMTTRN")
+    for sn in statementNodes:
+        n = getNodeText(sn, "NAME")
+        m = getNodeText(sn, "MEMO")
+        (n, m) = fixNameAndMemo(n, m)
+        setNodeText(sn,"NAME",n)
+        setNodeText(sn,"MEMO",m)
+
+    tree.write(file(mid, 'wb'))
+
+    # TODO write to fin
+    xq = XmlToQfx();
+    #xq.write(file(mid, 'rb'), file(fin, 'wb'))
 
 
 
