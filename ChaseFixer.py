cbf5ccca92437b89ef614a5329f22d45d6c05196
Darien Hager <darien+git@technofovea.com>
2014-02-04 21:03:24 -0800

    Added command-line arguments so that people don't need to hardcode paths etc.

--- ChaseFixer.py
+++ ChaseFixer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import tempfile
+import sys
 import io
 import re
 import os.path
@@ -66,4 +68,57 @@ class MyStatementFixer(AbstractStatementVisitor):
 
 __author__ = 'Darien Hager'
 if __name__ == "__main__":
-    pass # TODO command-line tool implemntation, content moved to TestRun
+
+    import argparse
+
+    # Set up command-line arguments
+    parser = argparse.ArgumentParser(description='Attempt to fix up a Chase QFX file')
+    parser.add_argument('src', type=argparse.FileType('r'),
+                        help='Filename of QFX file to read.')
+    parser.add_argument('dst', type=argparse.FileType('w'), nargs='?',
+                        default=sys.stdout,
+                        help='Filename of output file. If not present, prints to screen.')
+    parser.add_argument('--pause', dest='pause',
+                        action='store_true', default=False,
+                        help='If present, the program will pause before converting its temporary XML file back to QFX.')
+    parser.add_argument('--temp', type=str, nargs='?',
+                        default=None, dest='temp', metavar="PATH",
+                        help='Override location of temporary XML file.')
+    args = parser.parse_args()
+
+    # Read from command-line arguments and establish some of the defaults
+    if args.temp is None:
+        (handle, tempPath) = tempfile.mkstemp(suffix=".xml")
+        args.temp = tempPath
+
+    # Read QFX file
+    qx = QfxToXml()
+    qx.handleFile(args.src)
+
+    # Write to temporary XML
+    qx.write(file(args.temp, "wb"))
+
+    # Traverse XML file with fixup code
+    tree = ET.parse(args.temp)
+    root = tree.getroot()
+    walker = StatementWalker(root)
+    walker.walk(MyStatementFixer())
+
+    # Write our changes back to XML file
+    file(args.temp, "wb").write(ET.tostring(root))
+
+    # Maybe give user a chance to modify the XML before we translate it back
+    if args.pause:
+        print "Temporary file located at: " + args.temp
+        raw_input("Pausing in case you want to modify it with other tools. Press enter to continue...")
+        pass
+
+    # Convert XML to QFX and save the result
+    args.dst.write(xmlToQfxString(root))
+
+    # Future thoughts:
+    #
+    # Perhaps the best way to do this is to download the QFX **AND** the CSV and sythesize them, or possibly
+    # generate the QFX entirely from the CSV if applicable.
+    #
+    # First check whether QFX files are allowed to have longer strings, since it might not entirely be JPMC's fault
