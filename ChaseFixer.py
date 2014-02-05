0d44ec8c293c1e420768dbbbad5f8516394ce1f1
Darien Hager <darien+git@technofovea.com>
2014-02-04 20:27:05 -0800

    Moving batch/test code to its own file, so that main code can be a simpler command-line tool

--- ChaseFixer.py
+++ ChaseFixer.py
@@ -66,28 +66,4 @@ class MyStatementFixer(AbstractStatementVisitor):
 
 __author__ = 'Darien Hager'
 if __name__ == "__main__":
-    
-    qfxdir = "C:\\Users\\Darien\\Documents\\YNAB\\chase_export\\example\\checking\\"
-
-    src = os.path.join(qfxdir, "JPMC.qfx")
-    mid = os.path.join(qfxdir, "JPMC.xml")
-    dst = os.path.join(qfxdir, "JPMC_fixed.qfx")
-
-    qx = QfxToXml()
-    qx.handleFile(io.open(src, 'r', encoding='cp1252'))
-
-    qx.write(file(mid, "wb"))
-    tree = ET.parse(mid)
-    root = tree.getroot()
-    walker = StatementWalker(root)
-    walker.walk(MyStatementFixer())
-
-    file(mid, "wb").write(ET.tostring(root))
-    file(dst, "wb").write(xmlToQfxString(root))
-
-    # Future thoughts:
-    #
-    # Perhaps the best way to do this is to download the QFX **AND** the CSV and sythesize them, or possibly
-    # generate the QFX entirely from the CSV if applicable.
-    #
-    # First check whether QFX files are allowed to have longer strings, since it might not entirely be JPMC's fault
+    pass # TODO command-line tool implemntation, content moved to TestRun
