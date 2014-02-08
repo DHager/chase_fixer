c28035ddf7ee6a4a473a6a5f97ba76e7fe10ae50
Darien Hager <darien+git@technofovea.com>
2014-02-07 18:55:00 -0800

    Implement CSV reading, change fixer to preferentially use CSV description if present

--- ChaseFixer.py
+++ ChaseFixer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import copy
+import csv
 import tempfile
 import sys
 import re
@@ -8,20 +10,97 @@ import xml.etree.ElementTree as ET
 
 
 class CsvCorrelator(AbstractStatementVisitor):
-    def __init__(self, src, meta={}):
+    def __init__(self, src):
         super(CsvCorrelator, self).__init__()
+        self.qfxRe = re.compile(r'^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})\[(\d+):(\w+)]$')
         self.src = src
-        self.meta = meta
+        self.reader = csv.DictReader(src, delimiter=',', quotechar='"')
+        self.fieldNames = {
+            "DATE": "Post Date",
+            "DESC": "Description",
+            "AMT": "Amount"
+        }
+        self.rows = []
+        self.matchingRows = {}
+
+        for fname in self.fieldNames.values():
+            if fname not in self.reader.fieldnames:
+                raise Exception("CSV file is missing expected field '" + fname + "'")
+        self._loadCsvRows()
+
+    def _loadCsvRows(self):
+        while True:
+            try:
+                n = self.reader.next()
+                self.rows.append(n)
+            except StopIteration:
+                break
+
+    def _convertQfxDate(self, qdate):
+        matches = self.qfxRe.match(qdate)
+        if not matches:
+            raise Exception("Invalid QFX date encountered: " + qdate)
+        year = matches.group(1)
+        month = matches.group(2)
+        day = matches.group(3)
+
+        # To format expected in CSV
+        return month + "/" + day + "/" + year
+
+    def _findRow(self, date, amt, name, memo):
+        wsRe = re.compile(r'\s+')
+
+        for row in self.rows:
+            if row[self.fieldNames["DATE"]] != date:
+                continue
+            if float(row[self.fieldNames["AMT"]]) != amt:
+                continue
+
+            # Ignore case...
+            csvStr= row[self.fieldNames["DESC"]].lower()
+            qfxStr = (name + memo).lower()
+
+            # Ignore whitespace...
+            csvStr = re.sub(wsRe, '', csvStr)
+            qfxStr = re.sub(wsRe, '', qfxStr)
+
+            assert(len(qfxStr) <= len(csvStr))
+            # Compare that one is inside the other
+            if csvStr.find(qfxStr, 0, len(qfxStr)) < 0:
+                continue
+
+            return row
+
+        return None
 
     def visit(self, values, stmtNode):
-        pass
+
+        date = values.get("DTPOSTED", "00000000120000[0:GMT]")
+        date = self._convertQfxDate(date)
+
+        amt = float(values.get("TRNAMT", "0.0"))
+
+        row = self._findRow(
+            date,
+            amt,
+            values.get("NAME", ""),
+            values.get("MEMO", "")
+        )
+        if row is None:
+            self.matchingRows[stmtNode] = None
+            return  # Should this get logged?
+
+        self.matchingRows[stmtNode] = row
+
+    def getMatchedRows(self):
+        return copy.copy(self.matchingRows)
 
 
 class MyStatementFixer(AbstractStatementVisitor):
-    def __init__(self, meta={}):
+    def __init__(self, matchingRows={}):
         super(MyStatementFixer, self).__init__()
         self.debug = True
-        self.meta = meta
+        self.matchingRows = matchingRows
 
         self.regexes = [
             (
@@ -36,8 +115,8 @@ class MyStatementFixer(AbstractStatementVisitor):
             ),
             (
                 # Fix online bill-pay where the transaction ID is (for some reason) always part of the name field
-                re.compile(r"^Online Payment (\d{10}) To (\S+) (.*)$"),
-                lambda m: (m.group(2) + m.group(3), "Online payment " + m.group(1))
+                re.compile(r"^Online Payment (\d{10}) To (.*?) (\d\d/\d\d)$"),
+                lambda m: (m.group(2), "Online payment " + m.group(1) + " on " + m.group(3))
             ),
             (
                 # Fix the rare transfers where the transaction ID is too early and move it to the memo.
@@ -60,13 +139,25 @@ class MyStatementFixer(AbstractStatementVisitor):
         print "\n"
 
     def visit(self, values, stmtNode):
+        row = None
+        if stmtNode in self.matchingRows.keys():
+            row = self.matchingRows[stmtNode]
+
         name = values.get("NAME", "")
         memo = values.get("MEMO", "")
 
-        if len(name) > 32:
-            raise Exception("Unexpected name of >32 chars")
-
-        combined = name + " " + memo
+        if row is not None:
+            combined = row['Description']
+            # Just in case we don't match anything, re-implement Chase's splitting but
+            # without the whitespace trim or truncation
+            splitpoint = min(32, len(combined))
+            name = combined[0:splitpoint]
+            memo = combined[splitpoint:]
+            pass
+        else:
+            if len(name) > 32:
+                raise Exception("Unexpected name of >32 chars")
+            combined = name + " " + memo
 
         for (rex, func) in self.regexes:
             matches = rex.match(combined)
@@ -78,29 +169,7 @@ class MyStatementFixer(AbstractStatementVisitor):
         values["MEMO"] = memo
 
 
-__author__ = 'Darien Hager'
-if __name__ == "__main__":
-
-    import argparse
-
-    # Set up command-line arguments
-    parser = argparse.ArgumentParser(description='Attempt to fix up a Chase QFX file')
-    parser.add_argument('src', type=argparse.FileType('r'),
-                        help='Filename of QFX file to read, usually called JPMC.QFX')
-    parser.add_argument('dst', type=argparse.FileType('w'), nargs='?',
-                        default=sys.stdout,
-                        help='Filename of output file. If not present, prints to screen.')
-    parser.add_argument('--pause', dest='pause',
-                        action='store_true', default=False,
-                        help='If present, the program will pause before converting its temporary XML file back to QFX.')
-    parser.add_argument('--temp', type=str,
-                        default=None, dest='temp', metavar="PATH",
-                        help='Override location of temporary XML file.')
-    parser.add_argument('--csv', type=argparse.FileType('r'),
-                        default=None, dest='csvsrc', metavar="PATH",
-                        help='Optional CSV copy of the same transactions, which can be read for greater accuracy.'
-                             ' Usually called JPMC.CSV.')
-    args = parser.parse_args()
+def commandlineRun(args):
 
     # Read from command-line arguments and establish some of the defaults
     if args.temp is None:
@@ -119,14 +188,14 @@ if __name__ == "__main__":
     root = tree.getroot()
     walker = StatementWalker(root)
 
-    meta = {}  # For sharing information between visitors. Keyed by xml elements, payload is another dictionary.
+    matchedRows = {}
     if args.csvsrc is not None:
-        csv_visitor = CsvCorrelator(args.csvsrc, meta)  # Tries to acquire original name+memo string from JPMC.csv dumps
+        csv_visitor = CsvCorrelator(args.csvsrc)  # Tries to acquire original name+memo string from JPMC.csv dumps
         walker.walk(csv_visitor)
+        matchedRows = csv_visitor.getMatchedRows()
 
-    fix_visitor = MyStatementFixer(meta)  # Fix up name and memo values based on available information
+    fix_visitor = MyStatementFixer(matchedRows)  # Fix up name and memo values based on available information
     walker.walk(fix_visitor)
-
 
     # Write our changes back to XML file
     file(args.temp, "wb").write(ET.tostring(root))
@@ -146,3 +215,29 @@ if __name__ == "__main__":
     # generate the QFX entirely from the CSV if applicable.
     #
     # First check whether QFX files are allowed to have longer strings, since it might not entirely be JPMC's fault
+
+
+__author__ = 'Darien Hager'
+
+if __name__ == "__main__":
+    import argparse
+    # Set up command-line arguments
+    parser = argparse.ArgumentParser(description='Attempt to fix up a Chase QFX file')
+    parser.add_argument('src', type=argparse.FileType('r'),
+                        help='Filename of QFX file to read, usually called JPMC.QFX')
+    parser.add_argument('dst', type=argparse.FileType('w'), nargs='?',
+                        default=sys.stdout,
+                        help='Filename of output file. If not present, prints to screen.')
+    parser.add_argument('--pause', dest='pause',
+                        action='store_true', default=False,
+                        help='If present, the program will pause before converting its temporary XML file back to QFX.')
+    parser.add_argument('--temp', type=str,
+                        default=None, dest='temp', metavar="PATH",
+                        help='Override location of temporary XML file.')
+    parser.add_argument('--csv', type=argparse.FileType('r'),
+                        default=None, dest='csvsrc', metavar="PATH",
+                        help='Optional CSV copy of the same transactions, which can be read for greater accuracy.'
+                             ' Usually called JPMC.CSV.')
+    args = parser.parse_args()
+
+    commandlineRun(args);
